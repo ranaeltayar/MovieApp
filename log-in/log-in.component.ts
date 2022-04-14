13d846b13ae72098b7b68f1618c6c99c813c6cd0
ranaeltayar <102647193+ranaeltayar@users.noreply.github.com>
2022-04-14 21:35:00 +0200

    Create log-in.component.ts

---
+++ log-in/log-in.component.ts
@@ -0,0 +1,37 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { RouterModule, Router, ActivatedRoute} from '@angular/router';
+import { UserService } from '../users.service';
+
+
+
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css'],
+  providers:[UserService]
+})
+export class LogInComponent implements OnInit {
+  users:{email:string, password:any}[]=[];
+  constructor(private router: Router, private userservice:UserService,
+    private route: ActivatedRoute){ }
+
+  ngOnInit(): void {
+   this.users=this.userservice.users;
+  }
+
+
+  OnSubmit(form:NgForm){
+  if(form.value.email == this.users[0].email && form.value.password == this.users[0].password ){
+    this.userservice.login(); 
+    console.log('kiko');
+    this.router.navigate(["/catalog"], {relativeTo: this.route});
+     console.log('sabarnayarab');
+    
+   }
+
+  }
+
+}
